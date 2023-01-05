fd9c605f92d88c1b73b4a2f85820abab85c65e16
Clint Noorlander <clinton.noorlander@gmail.com>
2023-01-05 09:31:01 -0700

    work: prep3 date:1/5/23

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random number = new Random();
+        int magic_number = number.Next(1, 101);
+
+        Console.WriteLine("I am thinking of a magic number between 1 and 100");
+
+        string answer;
+
+        do
+        {
+            Console.WriteLine("");
+            Console.Write("What is your guess? ");
+            string guess_string = Console.ReadLine();
+            int guess = int.Parse(guess_string);
+
+            if (guess == magic_number)
+            {
+                answer = "correct";
+            }
+            else
+            {
+                answer = "incorrect";
+                if (guess < magic_number)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Try guessing higher");
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Try guessing lower");
+                }
+            }
+
+        } while (answer == "incorrect");
+
+        Console.WriteLine($"Congratulations! you guessed that the magic number was {magic_number}");
     }
 }
