94c33afce569ba37707ea7d9fa242db4075af6a4
Clint Noorlander <clinton.noorlander@gmail.com>
2023-01-04 20:45:25 -0700

    work done to prep1 and prep2 on 1/4/23

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int value = int.Parse(grade);
+
+        string letter = "";
+        string message = "";
+
+        if (value >= 90)
+        {
+            letter = "an A";
+            message = "Congratulations! You did it!";
+        }
+        else if (value >= 80)
+        {
+            letter = "a B";
+            message = "Congratulations! You did it!";
+        }
+        else if (value >= 70)
+        {
+            letter = "a C";
+            message = "Congratulations! You did it!";
+        }
+        else if (value >= 60)
+        {
+            letter = "a D";
+            message = "Close one! You'll get em next time champ.";
+        }
+        else
+        {
+            letter = "an F";
+            message = "Close one! You'll get em next time champ.";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+        Console.WriteLine($"{message}");
+
     }
 }
