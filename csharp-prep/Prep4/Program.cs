fcc3c2515d472e8bbcdfb52c5a94f506fda4c365
Clint Noorlander <clinton.noorlander@gmail.com>
2023-01-05 10:32:08 -0700

    work: prep4 date: 1/5/23

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        
-    }
+        Console.WriteLine("Please enter a series of numbers, enter 0 when finished:");
+
+        List<int> values = new List<int>();
+
+        string done;
+
+        do
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 0)
+            {
+                done = "yes";
+            }
+            else
+            {
+                values.Add(number);
+                done = "no";
+            }
+        }while (done == "no");
+
+        int size = values.Count;
+        int total = values.Sum();
+        int avg = total / size;
+        int max = values.Max();
+
+        Console.WriteLine("");
+
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {avg}");
+        Console.WriteLine($"The maximum is: {max}");
+        }
 }
