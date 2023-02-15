f39a1f0c898a577e53a1f6e3c3bdd260abcb542f
Clint Noorlander <clinton.noorlander@gmail.com>
2023-02-15 14:10:52 -0700

    eeeeee2

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Console.WriteLine("Hello Learning04 World!");
+
+        Assignment firstTest = new Assignment("Clinton Noorlander","Physics");
+        Console.WriteLine(firstTest.GetSummary());
+
+        MathAssignment mathTest = new MathAssignment("Clinton Noorlander", "Integration", "4.6", "9-20");
+        Console.WriteLine(mathTest.GetSummary());
+        Console.WriteLine(mathTest.GetHomeworkList());
+
+        WritingAssignment writingTest = new WritingAssignment("Clinton Noorlander", "Art History", "The History of Art Things");
+        Console.WriteLine(writingTest.GetSummary());
+        Console.WriteLine(writingTest.GetWritingInformation());
     }
 }
